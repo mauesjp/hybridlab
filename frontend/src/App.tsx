5580e5b2724df76dffbc3775c96f2db3b11f0c65
Jp <mauesjoao15@gmail.com>
2026-09-14 12:30:38 -0300

    feat: setup react frontend with vite and tailwind

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+    return (
+        <>
+            <h1> HybridLab</h1>
+            <p>Frontend Funcionando</p>
+            <h1 className="text-4xl font-bold">HybridLab</h1>
+        </>
+    )
+}
+
+export default App
